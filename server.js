c67f6c4525d326dddd499acfe27f39e4b45b8762
Jerry <exnxs@hotmail.com>
2017-06-09 11:09:33 -0400

    Prp repo for Heroku

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static('public'));
+const PORT = process.env.PORT || 3000;
 
-app.get('/', function(req, res){
-    res.send('Allo!');
+app.use(function(req, res, next){
+  if(req.headers['x-forwarded-proto'] === 'http') {
+    next();
+  } else {
+    res.redirect('http://' + req.hostname + req.url);
+  }
 });
 
-app.listen(3000, function(){
-  console.log("server started on port 3000");
+app.use(express.static('public'));
+
+app.listen(PORT, function(){
+  console.log("Express server is up on port " + PORT);
 });
