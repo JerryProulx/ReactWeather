7ff3337157d2ab445a5619b961c863a496aaf41c
Jerry <exnxs@hotmail.com>
2017-06-11 18:36:48 -0400

    Add app stylesheet

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,7 +3,7 @@ var React = require('react');
 var About = (props) => {
   return (
     <div className="row">
-      <h1 className="text-center">About</h1>
+      <h1 className="text-center page-title">About</h1>
       <p>React Weather is an app to know what is the current weather of a city. Juste use the search box, enter a city of your choice and let the app give you the info you need</p>
       <p>
         Here are some of the tools I used:
--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -21,7 +21,7 @@ var Nav = React.createClass({
         <div className="top-bar-right">
           <form onSubmit={this.onSearch}>
             <ul className="menu">
-              <li><input type="search" placeholder="Search Weather"/></li>
+              <li><input type="search" placeholder="Search Weather by city"/></li>
               <li><button type="submit" className="button">Get Weather</button></li>
             </ul>
           </form>
